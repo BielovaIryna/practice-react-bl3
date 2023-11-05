8248b05050499a398a113c5f772151e59d142f3d
Dmytro Sayko <saykodmytro@icloud.com>
2023-11-05 10:00:05 +0200

    up

--- src/components/WordsFilter/WordsFilter.jsx
+++ src/components/WordsFilter/WordsFilter.jsx
@@ -1,10 +1,11 @@
-
-import React from 'react'
+import React from 'react';
 
 export default function WordsFilter() {
   return (
-	<div>
-	
-	</div>
-  )
+    <div>
+      <label className={css.label}>
+        <input className={css.input} type="text" placeholder="Search" />
+      </label>
+    </div>
+  );
 }
